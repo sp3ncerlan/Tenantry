7788ceb7e3e522a349c3329c99cb4bc944e6430c
sp3ncerlan <lansp329@gmail.com>
2025-05-29 00:12:25 -0500

    Initial commit

---
+++ src/main/java/com/spencer/saasplatform/MultiTenantSaaSPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.spencer.saasplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiTenantSaaSPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiTenantSaaSPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spencer/saasplatform/MultiTenantSaaSPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spencer.saasplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiTenantSaaSPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
